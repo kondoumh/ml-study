b3360d7a9fda3283654f1eeda324db4fab55a8b5
kondoumh <kondoumh@gmail.com>
2018-10-24 00:19:18 +0900

    WIP chapter 2

--- zero/perceptron.py
+++ zero/perceptron.py
@@ -11,4 +11,26 @@ def AND(x1, x2):
         return 1
 
 
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
 print(f"{AND(0, 0)}, {AND(1, 0)}, {AND(0, 1)}, {AND(1, 1)}")
+print(f"{NAND(0, 0)}, {NAND(1, 0)}, {NAND(0, 1)}, {NAND(1, 1)}")
+print(f"{OR(0, 0)}, {OR(1, 0)}, {OR(0, 1)}, {OR(1, 1)}")
