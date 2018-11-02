f50bba7632b7c3d47f19146a1f2cd5ec61c63ef8
kondoumh <kondoumh@gmail.com>
2018-11-02 22:37:08 +0900

    add sigmoid_grad

---
+++ dl-from-scratch/plot/sigmoid_grad.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid_grad(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 0.5)
+plt.show()
