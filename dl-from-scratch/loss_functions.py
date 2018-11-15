63d846cecaba80d13d5f0b9dc62704526e17dcea
kondoumh <kondoumh@gmail.com>
2018-11-15 12:55:49 +0900

    add cross_entropy_error for batch function

--- dl-from-scratch/loss_functions.py
+++ dl-from-scratch/loss_functions.py
@@ -7,6 +7,15 @@ def cross_entropy_error(y, t):
     delta = 1e-7
     return -np.sum(t * np.log(y + delta))
 
+def cross_entropy_error_batch(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y + 1e-7)) / batch_size
+
+
 t = [0, 0, 1, 0, 0, 0, 0, 0, 0 ,0]
 y1 = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
 y2 = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
