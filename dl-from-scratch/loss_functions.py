4dcbbcb7bd0643593fc285e0ba5e7a546f7d99f6
kondoumh <kondoumh@gmail.com>
2018-11-15 13:06:44 +0900

    add cross_entrypy_error batch function for label data

--- dl-from-scratch/loss_functions.py
+++ dl-from-scratch/loss_functions.py
@@ -15,6 +15,13 @@ def cross_entropy_error_batch(y, t):
     batch_size = y.shape[0]
     return -np.sum(t * np.log(y + 1e-7)) / batch_size
 
+def cross_entropy_error_batch_label(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arrange(batch_size), t] + 1e-7)) / batch_size
 
 t = [0, 0, 1, 0, 0, 0, 0, 0, 0 ,0]
 y1 = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
