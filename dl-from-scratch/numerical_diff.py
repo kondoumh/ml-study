3ddca65d8e8f647f613d952e67234a19227c2b0b
kondoumh <kondoumh@gmail.com>
2018-12-03 13:16:02 +0900

    add partial diff function

--- dl-from-scratch/numerical_diff.py
+++ dl-from-scratch/numerical_diff.py
@@ -29,3 +29,13 @@ y2 = tf(x)
 plt.plot(x, y)
 plt.plot(x, y2)
 plt.show()
+
+def function_tmp1(x0):
+    return x0*x0 + 4.0**2.0
+
+def function_tmp2(x1):
+    return 3.0**2.0 + x1*x1
+
+print(numerical_diff(function_tmp1, 3.0))
+
+print(numerical_diff(function_tmp2, 4.0))
